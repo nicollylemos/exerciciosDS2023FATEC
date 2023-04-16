8c69bd362d26e20ed7802f897f1d2a426e1dee2b
Nicolly Lemos <102179005+nicollylemos@users.noreply.github.com>
2023-04-16 00:33:17 -0300

    Add files via upload

---
+++ SoCalcClasse/CalcClasse/Contas.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcClasse
+{  
+    /// <summary>
+    /// Classe para calcular valores
+    /// </summary>
+    public  class Contas
+    {
+        private double num1;
+        /// <summary>
+        /// 1º número do calculo 
+        /// </summary>
+
+
+        public double Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+
+        private double num2;
+        /// <summary>
+        /// 2º número do calculo
+        /// </summary>
+
+
+        public double Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+
+
+        public Contas()  //construtor padrão (alt + enter)
+        {
+        }
+
+        /// <summary>
+        /// Construtor alternativo passando valores 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        public Contas(double num1, double num2)   //gerar construtor
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+
+        /// <summary>
+        /// Calcula a soma dos valores informados 
+        /// </summary>
+        /// <returns> Retorna uma frase com o resultado </returns>
+
+        public string Somar(){
+
+            return "A soma é " + (Num1+Num2);
+        }
+
+        public  string Somar(double num1, double num2) {
+
+            return "A soma é " + (num1 + num2);
+        }
+
+
+
+        public string Subtrair()
+        {
+
+            return "A subtração é " + (Num1 - Num2);
+        }
+
+        public string Subtrair(double num1, double num2)
+        {
+
+            return "A subtração é " + (num1 - num2);
+        }
+
+
+        public string Multiplicacao()
+        {
+
+            return "A multiplicação é " + (Num1 * Num2);
+        }
+
+        public string Multiplicacao(double num1, double num2)
+        {
+
+            return "A multiplicação é " + (num1 * num2);
+        }
+
+        public string Divisao()
+        {
+
+            if (Num2 ==0)
+            {
+                return "Não é possível na dividir esse número por 0 ";
+            }
+
+            return "A divisão de " + (Num1 / Num2);
+        }
+
+        public string Divisao(double num1, double num2)
+        {
+
+            return "A divisão é " + (num1 / num2);
+        }
+
+
+        public string Sair()
+        {
+            return "\nVolte sempre!";
+        }
+        
+        // TODO terminar a classe
+    }
+}
---
+++ SoCalcClasse/CalcClasse/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalcClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Calculadora"; //valor (titulo)
+            Console.BackgroundColor = ConsoleColor.White; //mudar fundo
+            Console.ForegroundColor = ConsoleColor.DarkMagenta; //mudar letra
+
+            int opt = -1;
+            //double num1 = 0;
+            // double num2 = 0;
+
+            try   //tratar seção
+            {
+                do
+                {
+                    Contas contas = new Contas();
+                    Console.Clear(); //limpar tela (para colocar cor na tela inteira)
+                    Console.WriteLine(Console.Title); //puxando o valor (para MOSTRAR o titulo)
+                    Console.WriteLine("Operações" +
+                        "\n[1] Adição" +
+                        "\n[2] Subtração" +
+                        "\n[3] Multiplicação" +
+                        "\n[4] Divisão" +
+                        "\n[0] Sair" +
+                        "\nEscolha uma opção:");
+                    opt = Convert.ToInt32(Console.ReadLine()); /* ReadLine(); = para receber a escolha do usuario
+                Convert.ToInt32(Console.ReadLine()); = converter para int */
+                    string vResp;
+                    do{ 
+                    if (opt >= 1 && opt <= 4) // maior ou igual 1 e menor igual a 4
+                    {
+
+                        Console.Clear();
+                        Console.WriteLine("Informe o primeiro número:");
+                        contas.Num1 = double.Parse(Console.ReadLine()); // pegar os números reais e guardar na memória(variável)
+                        Console.WriteLine("Informe o segundo número:");
+                        contas.Num2 = double.Parse(Console.ReadLine());
+
+                    }
+                    switch (opt)// trabalhar com a variável opt
+                    {
+                        case 1:
+                            Console.WriteLine(contas.Somar());      // para priorizar e fazer uma conta utilizar ( ) na conta !!
+                            break; // cancelar repetição                     
+                        case 2:
+                            Console.WriteLine(contas.Subtrair());
+                            break;
+                        case 3:
+                            Console.WriteLine(contas.Multiplicacao());
+                            break;
+                        case 4:
+                            Console.WriteLine(contas.Divisao());
+                            break;
+                        case 0:
+                            Console.WriteLine(contas.Sair());
+                            Thread.Sleep(4000);
+                            break;
+                        default:
+                            Console.WriteLine("Opção Inválida!");
+                            Thread.Sleep(4000);
+                            opt = 5;
+                            break;
+                        }
+                        if(opt == 0)
+                        {
+                            break;
+                        }
+                        else if(opt== 5)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("\nDeseja repetir o procedimento?");
+                        vResp = Console.ReadLine();
+                    } while (vResp == "sim");
+                } while (opt != 0);
+              
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Aconteceu um erro: " + ex.Message);
+            }
+        }
+    }
+}
